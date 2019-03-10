4ced33cf45f9faa240f7c94e8db3129eb3257d9e
MechanicalPig <matteo.avellina@gmail.com>
2019-03-10 12:28:09 +0100

    change: update bootsrap script

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 # usr/bin/sh
 
+NOW=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+START_ME_UP_REPO="git@github.com:MechanicalPig/start-me-up.git"
+INSTALL_FOLDER="$HOME/tmp/start-me-up_$NOW"
+
 echo "---------------------------"
 echo "🚀  Time to start you up! 🚀"
 echo "---------------------------"
@@ -12,3 +16,18 @@ echo "✅  pip installed!"
 echo "⏳  Installing ansible..."
 pip install ansible
 echo "✅  ansible installed!"
+
+echo "👉  Temporary installation files will be stored here:"
+echo "\t$INSTALL_FOLDER"
+
+echo "⏳  Cloning git repository..."
+git clone $START_ME_UP_REPO $INSTALL_FOLDER
+echo "✅  repository cloned!"
+
+echo "🚿  Cleaning everything up..."
+rm -rf $INSTALL_FOLDER
+echo "✨  everything is clean!"
+
+echo "-------------------------------------"
+echo "You have been succesfully started up!"
+echo "Have fun! 👋"
